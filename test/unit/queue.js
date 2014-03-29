31b2c147dff5825f7b9f438e7a41e314a9221ae3
Pavel Machekhin <pavel.machekhin@gmail.com>
2014-03-29 15:26:48 +0200

    Base classes and first tests for storage module

---
+++ test/unit/queue.js
@@ -0,0 +1,16 @@
+var assert = require("assert")
+
+describe('Create job', function(){
+
+  describe("#createJob()", function() {
+
+  });
+
+  describe("#listJobs()", function() {
+
+  });
+
+  describe("#processJob()", function() {
+
+  });
+});
---
+++ test/unit/storage.js
@@ -0,0 +1,34 @@
+var assert = require("assert")
+  , fs = require("fs")
+  , os = require("os")
+  , QueueStorage = require("../../lib/queue/storage")
+
+describe('Queue Storage', function () {
+  describe('#setLocation()', function () {
+    it('should set temporary directory when path from param is not exists or not writeable', function () {
+      var storage = QueueStorage.create();
+
+      storage.setLocation(__dirname + '/notExistsDirectory');
+
+      assert.equal(os.tmpdir(), storage.getLocation());
+    });
+
+    it('should set temporary directory when path is not writeable', function () {
+      var storage = QueueStorage.create()
+        , path = '/bin';
+
+      storage.setLocation(path);
+
+      assert.equal(os.tmpdir(), storage.getLocation());
+    })
+
+    it('should set path from argument value when it is exists and writeable', function () {
+      var storage = QueueStorage.create()
+        , path = __dirname + '/../data';
+
+      storage.setLocation(path);
+
+      assert.equal(fs.realpathSync(path), storage.getLocation());
+    });
+  });
+});
